3a77528433fb6be651c52cde3172331e2628fb1c
Nicolas <nicolas.tvasquez@gmail.com>
2019-02-13 18:05:28 -0300

    sub

---
+++ HSpdv/HSpdv/MyContentPage.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Xamarin.Forms;
+
+namespace HSpdv
+{
+    public class MyContentPage :ContentPage
+    {
+        public MyContentPage()
+        {
+            var label = new Label
+            {
+                Text = "Escribe tu Nombre"
+            };
+            var texto = new Entry
+            {
+                Placeholder = "Escribe tu Nombre"
+            };
+            var btn = new Button
+            {
+                Text = "Holi"
+            };
+            btn.Clicked += (sender, e) =>
+            {
+                DisplayAlert("Mensaje", "Hola "+ texto.Text, "OK");
+            };
+            Content = new StackLayout
+            {
+                Padding = 30,
+                Spacing = 10,
+                Children = { label, texto ,btn}
+                
+            };
+        }
+    }
+}
